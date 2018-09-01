e5c87774eda95f46fa45f775e88441ecfe3624a6
dhackelman <drew.hackelman@gmail.com>
2018-08-31 20:43:05 -0400

    updating firebase access

--- src/index.js
+++ src/index.js
@@ -5,13 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import * as firebase from 'firebase';
 
-
-//
-// const database = firebase
-//   .initializeApp(config)
-//   .database()
-//   .ref();
-
+ firebase.initializeApp(config);
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
