9f8b1f332f111591a02ad45ba0ef67bd8b59d409
dhackelman <drew.hackelman@gmail.com>
2018-08-31 20:57:11 -0400

    updated with working authorization that isn't as visible?

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import * as firebase from 'firebase';
+import config from './config';
 
- firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
